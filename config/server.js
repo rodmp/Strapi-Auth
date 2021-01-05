3c776612eec0c7bdf08ddb32448425de1763ff77
rodmp <rodomiranda2020@gmail.com>
2021-01-05 10:11:07 -0800

    Create project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6930052fb6cb9cf670d63fd2946e4707'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28fa3e31-b716-43d7-a4b4-4d53ac7a88b3'
+};
